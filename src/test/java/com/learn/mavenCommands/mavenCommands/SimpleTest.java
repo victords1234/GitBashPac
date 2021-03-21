c99737b4137d3f616d24ce6570cd657fbf3cf0b1
victords1234 <victordsouza95@gmail.com>
2021-03-20 19:27:27 -0700

    Comitted trial

---
+++ src/test/java/com/learn/mavenCommands/mavenCommands/SimpleTest.java
@@ -0,0 +1,15 @@
+package com.learn.mavenCommands.mavenCommands;
+import org.testng.annotations.Test;
+public class SimpleTest 
+{
+	@Test(groups={"st"})
+	public void main()
+	{
+		System.out.println("Hello");
+	}
+	@Test(groups={"rt"})
+	public void mai()
+	{
+		System.out.println("Hello");
+	}
+}
